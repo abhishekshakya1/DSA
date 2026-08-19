c3128864bfcf62e175df4c7da44f4f42a0a4c59c
Abhishek Kumar Shakya <abhishekshakya2357@gmail.com>
2026-08-19 17:42:51 +0530

    Add warm up folder and basic programming concepts

---
+++ a-warm-up/programming.js
@@ -0,0 +1,43 @@
+console.log("HELLO WORLD !!");
+console.log("---------------------");
+
+
+let student = {
+    name: "Abhishek",
+    age: 18,
+    class: "BCA",
+    myArr: [2, 3, 4, 5, 6]
+};
+
+console.log(student);
+console.log(student.age);
+console.log(student.myArr);
+console.log("---------------------");
+
+
+const a = 10;
+const b = 20;
+const sum = a + b;
+const mul = a * b;
+
+console.log(sum);
+console.log("---------------------");
+console.log(mul);
+console.log("---------------------");
+
+let firstName = "Abhishek";
+let lastName = "Shakya";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+console.log("---------------------");
+
+
+let arr = [2, 10, 30, 40, 50];
+
+console.log(arr[2][0]);
+console.log(arr[7]);
+console.log(arr);
+
+console.log("---------------------");
+
